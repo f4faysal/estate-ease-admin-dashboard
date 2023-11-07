0fc6feafe03573e92637e894766bed07494fc765
f4faysal <f4faysals@gmail.com>
2023-11-07 10:31:29 +0600

    Dashbord design

--- app/(dashboard)/[adminId]/(routes)/profile/page.tsx
+++ app/(dashboard)/[adminId]/(routes)/profile/page.tsx
@@ -1,7 +1,17 @@
+import { Metadata } from "next";
+import MyProfile from "./components/MyProfile";
+
+export const metadata: Metadata = {
+  title: "My Profile",
+  description: "Manage your profile and details.",
+};
+
 const ProfilePage = () => {
   return (
-    <div>
-      <h1>Profile Page</h1>
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <MyProfile />
+      </div>
     </div>
   );
 };
